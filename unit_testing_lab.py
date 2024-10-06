6d9c1b888fb96dccd87552533e1f0cb725bbc707
abdullahrizmi14 <abdullahrizmi14@gmail.com>
2024-10-06 21:13:27 +0100

    adding a unit test message

--- unit_testing_lab.py
+++ unit_testing_lab.py
@@ -1,6 +1,14 @@
+####### Step 1 : Function Design 
+
+
+# function below adds 1 to any number
 def add(a):
     result = a + 1
     return result
 
 result = add(5)
 print(result)
+
+# Unit tests
+
+## A unit test would be to ensure the number entered is not a string
