2011a9d801c9f47b9ef6ed1d70fa23f2e9132d4a
abdullahrizmi14 <abdullahrizmi14@gmail.com>
2024-10-11 11:04:35 +0100

    first_writeup

--- pseudocode_trial.py
+++ pseudocode_trial.py
@@ -1,1 +1,26 @@
-print("hey new world")
+import random
+
+
+N_min = 1
+N_max = 100
+
+N_rand = random.randint(N_min,N_max)
+
+i = 1
+y = 1
+N_guess = input("Guess a number")
+
+while (y = 1):
+    N_guess = input("Guess a number")
+    if (N_guess > N_rand):
+        print("Too high")
+        i += 1
+    
+    elif (N_guess < N_rand):
+        print("Too Low")
+        i += 1
+    
+    else:
+        print("Success! Guessed in " + i + " attempts")
+        y = 0
+    
