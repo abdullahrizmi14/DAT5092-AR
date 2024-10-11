883bd0ab3f6ad3b988186dd5c1447ca57fa7450d
abdullahrizmi14 <abdullahrizmi14@gmail.com>
2024-10-11 11:41:11 +0100

    pseudocode works

--- pseudocode_trial.py
+++ pseudocode_trial.py
@@ -10,7 +10,7 @@ i = 1
 y = 1
 
 while (y == 1):
-    N_guess = int(input("Guess a number"))
+    N_guess = int(input("Guess a number "))
     if (N_guess > N_rand):
         print("Too high")
         i += 1
@@ -20,6 +20,6 @@ while (y == 1):
         i += 1
     
     else:
-        print("Success! Guessed in " + i + " attempts")
+        print("Success! Guessed in " + str(i) + " attempts")
         y = 0
     
