20370ee4537cdab27539731d43af4fd177673d1c
abdullahrizmi14 <abdullahrizmi14@gmail.com>
2024-10-11 15:04:17 +0100

    pseudocode_unit_test_first

---
+++ unit_testing_psuedocode.py
@@ -0,0 +1,12 @@
+import unittest
+
+## Unit Tests for psuedocode
+
+class TestFunction(unittest.TestCase);
+    
+    def y_starting(self):
+        starting_y = 1
+        self.assertEqual(starting_y,1)
+    
+if __name__ == '__main__':
+    unittest.main()
